2eaa74b361a7f41bd44640b9319c99faf825b534
Wafy80 <119013510+Wafy80@users.noreply.github.com>
2024-06-18 00:41:21 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/acestream/playlist
-set -m
-gunicorn -w 2 -b 0.0.0.0:6880 --access-logfile search.log search:app &
-/opt/acestream/start-engine --client-console --bind-all
-fg %1
