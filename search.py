a2ef4f2f543025fa64cd28f25e58676ae3e66f00
Wafy80 <wafy80@gmail.com>
2024-06-19 23:50:51 +0200

    changed link url

--- search.py
+++ search.py
@@ -30,7 +30,7 @@ def get_args():
 
 @app.route('/')
 def home():
-    return 'Put this link into your favourite IPTV player:<br>' + request.base_url + 'search.m3u'
+    return 'Put this link into your favourite IPTV player:<br>' + request.root_url + 'search.m3u'
 
 @app.route('/log')
 def log():
